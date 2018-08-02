8a084473237fda17c7a54c87f3fe4296caf4f638
Nikhil Raina <nxr5013@g.rit.edu>
2018-08-02 17:45:37 -0500

    Finished Commenting.
    Ready for Testing.

--- Data_Directory/csvConverter.py
+++ Data_Directory/csvConverter.py
@@ -1,26 +1,30 @@
 import csv
 from APIgetData import getData
-import collections
 
 
 def CSV_file_getData(webURL, token):
     """
-
-    :param webURL:
-    :param token:
-    :return:
+    Function responsible to get the API report data and call the necessary functions
+    to make the CSV File.
+    :param webURL: The URL of the of the main Home Page
+    :param token: The token that will be sent as a header.
+            Usually indicates that permission has been given to the given party or
+            individual who is using the data.
+    :return: None
     """
-    # webAPI_data = dict()
     webAPI_data = getData(webURL, token)
-    heading_1 = ['Dept', 'Category', 'QuestionID', 'ParentQuestionID',  # the category ID, I think????
+    heading_1 = ['Dept', 'Category', 'QuestionID', 'ParentQuestionID',
                  'Question', 'Answer', 'ScoreActual', 'ScorePossible',
                  'Comment']
-
-    key_list = list(webAPI_data.keys())
     all_list = list()
-    for i in range(0, len(key_list)):
-        all_list.append(webAPI_data[key_list[i]])
+    for key in webAPI_data:
+        all_list.append(webAPI_data[key])
 
+    # ========================================================== #
+    # ========================================================== #
+    # Creates a dictionary where all the departments in the different
+    # reports are the keys, using open addressing for each of their
+    # separates reports.
     newData_dict = dict()
     for dictionary in all_list:
         for key in dictionary:
@@ -28,12 +32,13 @@ def CSV_file_getData(webURL, token):
                 newData_dict.setdefault(key, list())
             newData_dict[key].append(dictionary[key])
 
-    result = False
-    # sort_data = collections.OrderedDict(sorted(newData_dict.items()))
-    with open('Report_Output_Template.csv', 'w', newline='') as report_file:
-        #for key in sort_data.items():
+    # ========================================================== #
+    # ========================================================== #
+    # Creates a CSV File. Calls the makeCSV function each departmental value
+    with open('Report_Output.csv', 'w', newline='') as report_file:
+        csvWriter = csv.writer(report_file)
+        csvWriter.writerow(heading_1)
         for key in newData_dict:
-            #for i in range(0, len(newData_dict[key])):
             for i in range(0, len(newData_dict[key])):
                 cat_que_list = (newData_dict[key][i][2])
                 for cat in cat_que_list:
@@ -43,34 +48,28 @@ def CSV_file_getData(webURL, token):
                     parentQuestionID = cat_que_list[cat][4]
                     questions = cat_que_list[cat][3]
                     ans_list = cat_que_list[cat][5]
-                    makeCSV(dept, category, questionID, parentQuestionID, questions,
-                            ans_list, heading_1, result, report_file)
-                    result = True
 
+                    # Function call
+                    makeCSV(dept, category, questionID, parentQuestionID, questions, ans_list, csvWriter)
     report_file.close()
 
 
-def makeCSV(dept, category, questionID, parentQuestionID, questions, ans_list, heading_1, result, report_file):
+def makeCSV(dept, category, questionID, parentQuestionID, questions, ans_list, csvWriter):
     """
-
-    :param dept:
-    :param category:
-    :param questionID:
-    :param parentQuestionID:
-    :param questions:
-    :param ans_list:
-    :param heading_1:
-    :param result:
-    :param report_file:
-    :return:
+    Takes in multiple parameters and writes a set number of rows, in the
+    CSV File, depending on the number of elements present in parentQuestionID.
+    :param csvWriter: Object for the CSV Writer
+    :param dept: Stores the Department
+    :param category: Stores the Category
+    :param questionID: Stores a list containing Question IDs
+    :param parentQuestionID: Stores a list containing Parent Question IDs
+    :param questions: Stores a list containing Questions
+    :param ans_list: Stores a Dictionary from the Answer key from: (reports/ID/unit_questions)
+    :return: None
     """
-    csvwriter = csv.writer(report_file)
-    if not result:
-        csvwriter.writerow(heading_1)
 
     # parentQuestionID, questionID, ans_list and questions
     # have the same number of elements in them
-
     for i in range(0, len(parentQuestionID)):
         write = list()
         ans_dict = ans_list[i]
@@ -88,29 +87,7 @@ def makeCSV(dept, category, questionID, parentQuestionID, questions, ans_list, h
         write.append(scorePossible)         # H
         write.append(comment)               # I
 
-        csvwriter.writerow(write)
-
-    var = 1
+        csvWriter.writerow(write)
 
 
 CSV_file_getData("https://demo.isora.saltycloud.com/", "c548a5524615454ac53281ac01efd56bbf69f4d9")
-
-#
-# def file1(report_file):
-#     csvwriter = csv.writer(report_file)
-#     heading_1 = ['Question', 'Answer', 'ScoreActual', 'ScorePossible','Comment']
-#     csvwriter.writerow(heading_1)
-#
-#
-# def file2(report_file):
-#     csvwriter = csv.writer(report_file)
-#     heading_1 = ['Dept', 'Category', 'QuestionID', 'ParentQuestionID',  # the category ID, I think????
-#                  'Question', 'Answer', 'ScoreActual', 'ScorePossible',
-#                  'Comment']
-#     csvwriter.writerow(heading_1)
-#
-#
-# with open('Report_Output_Template.csv', 'w', newline='') as report_file:
-#     file1(report_file)
-#     file2(report_file)
-#     report_file.close()
