dc55df5c06f04078d52af96bd5fde6da5f7eb4e4
Nikhil Raina <nxr5013@g.rit.edu>
2018-08-04 11:58:52 -0500

    Added Assessment category. Made changes to the code to incorporate "Assessment".
    Ready for Testing.

--- Data_Directory/csvConverter.py
+++ Data_Directory/csvConverter.py
@@ -13,24 +13,9 @@ def CSV_file_getData(webURL, token):
     :return: None
     """
     webAPI_data = getData(webURL, token)
-    heading_1 = ['Dept', 'Category', 'QuestionID', 'ParentQuestionID',
+    heading_1 = ['Assessment','Dept', 'Category', 'QuestionID', 'ParentQuestionID',
                  'Question', 'Answer', 'ScoreActual', 'ScorePossible',
                  'Comment']
-    all_list = list()
-    for key in webAPI_data:
-        all_list.append(webAPI_data[key])
-
-    # ========================================================== #
-    # ========================================================== #
-    # Creates a dictionary where all the departments in the different
-    # reports are the keys, using open addressing for each of their
-    # separates reports.
-    newData_dict = dict()
-    for dictionary in all_list:
-        for key in dictionary:
-            if not key in newData_dict:
-                newData_dict.setdefault(key, list())
-            newData_dict[key].append(dictionary[key])
 
     # ========================================================== #
     # ========================================================== #
@@ -38,26 +23,29 @@ def CSV_file_getData(webURL, token):
     with open('Report_Output.csv', 'w', newline='') as report_file:
         csvWriter = csv.writer(report_file)
         csvWriter.writerow(heading_1)
-        for key in newData_dict:
-            for i in range(0, len(newData_dict[key])):
-                cat_que_list = (newData_dict[key][i][2])
-                for cat in cat_que_list:
-                    dept = key
-                    category = cat_que_list[cat][0]
-                    questionID = cat_que_list[cat][2]
-                    parentQuestionID = cat_que_list[cat][4]
-                    questions = cat_que_list[cat][3]
-                    ans_list = cat_que_list[cat][5]
+        for outer_key in webAPI_data:
+            for inner_key in webAPI_data[outer_key]:
+                for i in range(0, len(webAPI_data[outer_key][inner_key])):
+                    val_dict = webAPI_data[outer_key][inner_key][2]
+                    for cat in val_dict:
+                        assessment = outer_key
+                        dept = inner_key
+                        category = val_dict[cat][0]
+                        questionID = val_dict[cat][2]
+                        parentQuestionID = val_dict[cat][4]
+                        questions = val_dict[cat][3]
+                        ans_list = val_dict[cat][5]
 
-                    # Function call
-                    makeCSV(dept, category, questionID, parentQuestionID, questions, ans_list, csvWriter)
+                        # Function call
+                        makeCSV(assessment, dept, category, questionID, parentQuestionID, questions, ans_list, csvWriter)
     report_file.close()
 
 
-def makeCSV(dept, category, questionID, parentQuestionID, questions, ans_list, csvWriter):
+def makeCSV(assessment, dept, category, questionID, parentQuestionID, questions, ans_list, csvWriter):
     """
     Takes in multiple parameters and writes a set number of rows, in the
     CSV File, depending on the number of elements present in parentQuestionID.
+    :param assessment: The assessment category
     :param csvWriter: Object for the CSV Writer
     :param dept: Stores the Department
     :param category: Stores the Category
@@ -77,15 +65,16 @@ def makeCSV(dept, category, questionID, parentQuestionID, questions, ans_list, c
         scoreActual = ans_dict['value']
         scorePossible = ans_dict['max_value']
         comment = ans_dict['details']
-        write.append(dept)                  # A
-        write.append(category)              # B
-        write.append(questionID[i])         # C
-        write.append(parentQuestionID[i])   # D
-        write.append(questions[i])          # E
+        write.append(assessment)            # A
+        write.append(dept)                  # B
+        write.append(category)              # C
+        write.append(questionID[i])         # D
+        write.append(parentQuestionID[i])   # E
+        write.append(questions[i])          # F
         write.append(answer)                # G
-        write.append(scoreActual)           # G
-        write.append(scorePossible)         # H
-        write.append(comment)               # I
+        write.append(scoreActual)           # H
+        write.append(scorePossible)         # I
+        write.append(comment)               # J
 
         csvWriter.writerow(write)
 
